2c1011f9365769046374e901e69f5a04cf934bb0
Tran Luong <tranluong@Trans-MacBook-Air.local>
2021-05-15 20:01:00 -0700

    test project layout

--- src/components/PortfolioPreview.jsx
+++ src/components/PortfolioPreview.jsx
@@ -1,16 +1,28 @@
-import React from 'react'
+import {useState, useEffect} from 'react'
 import PortfolioCard from "./Card/PortfolioCard.jsx"
 import {data} from "../data.js"
 import Carousel from "react-material-ui-carousel"
 import "./styles/Portfolio.css"
+
 export default function PortfolioPreview() {
+    let [content, setContent] = useState([])
+    useEffect(() => {
+        fetch()
+    }, [])
+
+    function fetch() {
+        setContent(data)
+    }
+    
     return (
-    <Carousel className="project-list">
-            {data.map((item) => {
+    <div className="project-list">
+            {/* <Carousel className="project-list"> */}
+
+            {content.map((item) => {
                 return (
                     <PortfolioCard key={item.number} data={item}/>
                 )
             })} 
-        </Carousel>
+        </div>
     )
 }
