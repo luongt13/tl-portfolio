cb94c093d8eeed9f501c91c906a1e17c5f4e23ac
Tran Luong <65784084+luongt13@users.noreply.github.com>
2022-05-17 12:38:27 -0400

    add work projects (#8)
    Co-authored-by: Tran Luong <tranluong@Trans-MacBook-Air.local>

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,26 +1,45 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import PortfolioPreview from "../PortfolioPreview/PortfolioPreview.jsx"
 import About from "../About/About.jsx"
 import Contact from "../Contact/Contact.jsx"
 import Intro from "../Intro/Intro.jsx"
-
+import { projects, workProjects } from "../../data.js"
+ 
 import "./Landing.css"
 export default function Landing() {
-    return (
-        <div className="content">
-            <div className="main" id="landing">
-                <Intro/>
-            </div>
-            <div className="main" id="about">
-                <About/>
-            </div>
-            <div className="main" id="projects">
-                <h2 className="color-label">Things I've created.</h2>
-                <PortfolioPreview/>
-            </div>
-            <div className="main" id="contact">
-                <Contact/>
-            </div>
-        </div>
-    )
+   const [projectData, setProjectData] = useState([])
+   const [workProjectData, setWorkProjectData] = useState([])
+ 
+   useEffect(() => {
+       fetchProjects()
+       fetchWorkProjects()
+   }, [])
+ 
+   const fetchProjects = () => {
+       setProjectData(projects)
+   }
+ 
+   const fetchWorkProjects = () => {
+       setWorkProjectData(workProjects)
+   }
+ 
+   return (
+       <div className="content">
+           <div className="main" id="landing">
+               <Intro/>
+           </div>
+           <div className="main" id="about">
+               <About/>
+           </div>
+           <div className="main" id="projects">
+               <h2 className="color-label">Things I've created for work.</h2>
+               <PortfolioPreview projects={workProjectData}/>
+               <h2 className="color-label">Things I've created for fun.</h2>
+               <PortfolioPreview projects={projectData}/>
+           </div>
+           <div className="main" id="contact">
+               <Contact/>
+           </div>
+       </div>
+   )
 }
