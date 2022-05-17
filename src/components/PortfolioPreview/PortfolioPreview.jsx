048d48f797c8c08d5919146fb8b1f5148b9038ed
Tran Luong <65784084+luongt13@users.noreply.github.com>
2022-05-17 12:41:10 -0400

    add work projects (#8) (#9)
    Co-authored-by: Tran Luong <tranluong@Trans-MacBook-Air.local>
    
    Co-authored-by: Tran Luong <tranluong@Trans-MacBook-Air.local>

--- src/components/PortfolioPreview/PortfolioPreview.jsx
+++ src/components/PortfolioPreview/PortfolioPreview.jsx
@@ -1,26 +1,18 @@
-import {useState, useEffect} from 'react'
+import React from "react"
 import PortfolioCard from "../Card/PortfolioCard.jsx"
-import {data} from "../../data.js"
 import "./Portfolio.css"
-
-export default function PortfolioPreview() {
-    let [content, setContent] = useState([])
-    useEffect(() => {
-        fetch()
-    }, [])
-
-    function fetch() {
-        setContent(data)
-    }
-
-    return (
-    <div className="project-list">
-            {/* <h2 className="color-label">Things I've created.</h2> */}
-            {content.map((item) => {
-                return (
-                    <PortfolioCard key={item.number} data={item}/>
-                )
-            })} 
-        </div>
-    )
+ 
+export default function PortfolioPreview(props) {
+   const { projects } = props
+ 
+ 
+   return (
+   <div className="project-list">
+           {projects && projects.map((item) => {
+               return (
+                   <PortfolioCard key={item.number} data={item}/>
+               )
+           })}
+       </div>
+   )
 }
--- src/data.js
+++ src/data.js
@@ -1,38 +1,59 @@
-export const data = [
-        {
-            "number": 1,
-            "title": "Meal Time Insulin",
-            "description": "an application to calculate total carbohydrates and insulin to carbohydrate ratio",
-            "webURL": "https://luongt13.github.io/meal-time-insulin/",
-            "githubURL": "https://github.com/luongt13/meal-time-insulin",
-            "img": "/images/mealtime-sm.gif",
-            "tools": "JavaScript, axios, HTML/CSS, REST API"
-        },
-        {
-            "number": 2,
-            "title": "Fish Goals",
-            "description": "a goal setting application",
-            "webURL": "https://fish-goals.netlify.app/",
-            "githubURL": "https://github.com/luongt13/fishgoal",
-            "img": "/images/fishgoals-sm.gif",
-            "tools": "React, axios, AirTable, Material-UI, REST API"
-        },
-        {
-            "number": 3,
-            "title": "devOcean",
-            "description": "a community based messaging application",
-            "webURL": "https://devocean-app.netlify.app/",
-            "githubURL": "https://github.com/luongt13/devOcean-message",
-            "img": "/images/devocean-sm.gif",
-            "tools": "React, axios, Mongoose, MongoDB, Express"
-        },
-        {
-            "number": 4,
-            "title": "weTinerary",
-            "description": "an itinerary sharing application",
-            "webURL": "https://we-tinerary.netlify.app/",
-            "githubURL": "https://github.com/luongt13/weTinerary",
-            "img": "/images/wetinerary-sm.gif",
-            "tools": "React, axios, Ruby, Ruby on Rails, PostgresQL"
-        }
-    ]
+export const projects = [
+    {
+        "number": 1,
+        "title": "Meal Time Insulin",
+        "description": "an application to calculate total carbohydrates and insulin to carbohydrate ratio",
+        "webURL": "https://luongt13.github.io/meal-time-insulin/",
+        "githubURL": "https://github.com/luongt13/meal-time-insulin",
+        "img": "/images/mealtime-sm.gif",
+        "tools": "JavaScript, axios, HTML/CSS, REST API"
+    },
+    {
+        "number": 2,
+        "title": "Fish Goals",
+        "description": "a goal setting application",
+        "webURL": "https://fish-goals.netlify.app/",
+        "githubURL": "https://github.com/luongt13/fishgoal",
+        "img": "/images/fishgoals-sm.gif",
+        "tools": "React, axios, AirTable, Material-UI, REST API"
+    },
+    {
+        "number": 3,
+        "title": "devOcean",
+        "description": "a community based messaging application",
+        "webURL": "https://devocean-app.netlify.app/",
+        "githubURL": "https://github.com/luongt13/devOcean-message",
+        "img": "/images/devocean-sm.gif",
+        "tools": "React, axios, Mongoose, MongoDB, Express"
+    },
+    {
+        "number": 4,
+        "title": "weTinerary",
+        "description": "an itinerary sharing application",
+        "webURL": "https://we-tinerary.netlify.app/",
+        "githubURL": "https://github.com/luongt13/weTinerary",
+        "img": "/images/wetinerary-sm.gif",
+        "tools": "React, axios, Ruby, Ruby on Rails, PostgresQL"
+    }
+]
+
+export const workProjects = [
+    {
+        "number": 1,
+        "title": "Crosscut",
+        "description": "a microplanning application",
+        "webURL": "https://app.crosscut.io",
+        "githubURL": null,
+        "img": "/images/crosscut.gif",
+        "tools": "Vue, ky, AWS, Mapbox, AWS Amplify"
+    },
+    {
+        "number": 2,
+        "title": "DHIS2",
+        "description": "create catchments for health facilities",
+        "webURL": null,
+        "githubURL": "https://github.com/crosscutio/dhis2-crosscut-app",
+        "img": "/images/dhis.gif",
+        "tools": "React, AWS Amplify, DHIS2 tools"
+    }
+]
